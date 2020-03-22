8a55575b3a0ec6f0a48298c4082f762745925f78
Theohr <47179221+Theohr@users.noreply.github.com>
2020-03-22 22:47:43 +0000

    Created a timer that runs from 00:00 on start of the level, restarts on restart button click and stops when the user completes the puzzle in Easy Mode

---
+++ Bridges/Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart;
+    // reference to the text in the game
+    public Text textBox;
+    public float speed = 1;
+    bool runningTimer = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        // getting the component of the text box of the timer (00:00)
+        textBox = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the timer is running then we enter the if statement
+        if (runningTimer)
+        {
+            // timer increments 1 second per frame
+            timeStart += Time.deltaTime * speed;
+            // each minute are added every 59 seconds and goes up to 59 minutes
+            string minutes = Mathf.Floor((timeStart % 3600) / 59).ToString("00");
+            // each second gets increment up to 59 
+            string seconds = (timeStart % 59).ToString("00");
+            // text box displaying the timer in game
+            textBox.text = minutes + ":" + seconds;
+        }
+    }
+
+    // Method that stops the timer if the User is done with the puzzle
+    public void StopTimer()
+    {
+        // setting the bool var to false so timer can stop running
+        runningTimer = false;
+    }
+}
