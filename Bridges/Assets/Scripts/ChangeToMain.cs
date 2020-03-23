61270eb9cf7e987ee7e5c2d7ae5749121770b466
Theohr <47179221+Theohr@users.noreply.github.com>
2020-03-23 22:49:13 +0000

    Created a Home Page with Buttons that transfer player into the represented mode according to the name, Fixed the Change diff in Easy mode to go back to Home Page, Also added a Quit button

---
+++ Bridges/Assets/Scripts/ChangeToMain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeToMain : MonoBehaviour
+{
+    // Change to Main Scene
+    public void ChangeToMainScene()
+    {
+        SceneManager.LoadScene("HomePage");
+    }
+}
---
+++ Bridges/Assets/Scripts/CloseGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGame : MonoBehaviour
+{
+    // Close the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
